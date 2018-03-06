bbe0f192450ec7f6b7a68a4d4bda9a6dd8a05034
Carlos Henche <carlos.henche@digibis.com>
2018-03-06 09:27:03 +0100

    Formateo de código

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5webapp.model;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
 @Entity
 public class Book {
     @Id
@@ -13,9 +14,9 @@ public class Book {
     private String publisher;
 
     @ManyToMany
-    @JoinTable(name="author_book",
-            joinColumns =@JoinColumn(name="book_id"),
-            inverseJoinColumns = @JoinColumn(name="author_id"))
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book(String title, String isbn, String publisher) {
